c27dedc21d911a275cfe928b7d09ef9f6b66bd9f
burakonce9 <burakonce9@gmail.com>
2023-08-29 10:10:27 +0300

    portfolio

---
+++ Portfolio/js/communication.js
@@ -0,0 +1,9 @@
+function showAboutPageButton(){
+  window.location.href="about.html";
+}
+function showMainPageButton(){
+  window.location.href="index.html";
+}
+function showCommunicationPageButton(){
+  window.location.href="communication.html";
+}
